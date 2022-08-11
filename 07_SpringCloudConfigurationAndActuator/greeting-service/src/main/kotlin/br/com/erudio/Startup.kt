2361d854f8799f39057104db6d7954d48cbed5c2
leandrocgsi <leandrocgsi@gmail.com>
2022-08-11 19:57:55 -0300

    Working with Spring Cloud Configuration and Spring Boot Actuator

--- 07_SpringCloudConfigurationAndActuator/greeting-service/src/main/kotlin/br/com/erudio/Startup.kt
+++ 07_SpringCloudConfigurationAndActuator/greeting-service/src/main/kotlin/br/com/erudio/Startup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GreetingServiceApplication
+class Startup
 
 fun main(args: Array<String>) {
-	runApplication<GreetingServiceApplication>(*args)
+	runApplication<Startup>(*args)
 }
