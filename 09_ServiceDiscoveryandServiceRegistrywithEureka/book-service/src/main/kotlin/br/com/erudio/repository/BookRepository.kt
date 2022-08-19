c94088b3517615f6cd092f309c8c0ae605fe5bf7
leandrocgsi <leandrocgsi@gmail.com>
2022-08-19 16:01:34 -0300

    Service Discovery and Service Registry with Zookeeper - Book Service

--- 09_ServiceDiscoveryandServiceRegistrywithEureka/book-service/src/main/kotlin/br/com/erudio/repository/BookRepository.kt
+++
@@ -1,6 +0,0 @@
-package br.com.erudio.repository
-
-import br.com.erudio.model.Book
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BookRepository : JpaRepository<Book, Long?>
