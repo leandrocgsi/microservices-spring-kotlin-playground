514e62cc148090bd5defbbd1150c5b810ea99d61
leandrocgsi <leandrocgsi@gmail.com>
2022-08-20 22:30:32 -0300

    Service Discovery and Service Registry with Zookeeper

--- 09_ServiceDiscoveryandServiceRegistrywithZookeeper/book-service/src/main/kotlin/br/com/erudio/Startup.kt
+++ 09_ServiceDiscoveryandServiceRegistrywithZookeeper/book-service/src/main/kotlin/br/com/erudio/Startup.kt
@@ -2,10 +2,13 @@ package br.com.erudio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 class Startup
 
 fun main(args: Array<String>) {
